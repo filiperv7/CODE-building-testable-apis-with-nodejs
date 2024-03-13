7a81c20026a2147cd1b6d3bd4a88d3bb62e49659
Filipe Rodrigo <filipevasconcelos91.rodrigo@gmail.com>
2024-03-13 17:48:32 -0300

    implementing password hashing with bcrypt

--- src/models/user.js
+++ src/models/user.js
@@ -1,10 +1,27 @@
+import bcrypt from 'bcrypt'
 import mongoose from 'mongoose'
+import Util from 'util'
+
+const hashAsync = Util.promisify(bcrypt.hash)
 
 const schema = new mongoose.Schema({
   name: String,
   email: String,
   password: String,
   role: String
+})
+
+schema.pre('save', async function (next) {
+  if (!this.password || !this.isModified('password')) {
+    return next()
+  }
+
+  try {
+    const hashedPassword = await hashAsync(this.password, 10)
+    this.password = hashedPassword
+  } catch (error) {
+    next(err)
+  }
 })
 
 schema.set('toJSON', {
